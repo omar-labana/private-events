bcacfe06093d2f91ad040a8935d1005ff56e738d
Omar Labana <omarlabana@outlook.com>
2021-07-11 23:22:06 +0200

    Update events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,69 +1,47 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index]
 
-  # GET /events or /events.json
-  def index
-    @events = Event.all
-  end
-
-  # GET /events/1 or /events/1.json
-  def show
-  end
-
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
-  def edit
-  end
-
-  # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
-
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: "Event was successfully created." }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /events/1 or /events/1.json
+  def index
+    @events = Event.all.order(:date)
+    @users = User.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @user = User.find(@event.creator_id)
+    
+  end
+
+  def edit
+    
+  end
+
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: "Event was successfully updated." }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    @event = Event.find(params[:id])
+   
   end
 
-  # DELETE /events/1 or /events/1.json
   def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    Event.find(params[:id]).delete
+    redirect_to root_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def event_params
-      params.require(:event).permit(:name, :description, :date, :location)
-    end
+  def event_params
+    params.require(:event).permit(:description, :date, :location)
+  end
 end
