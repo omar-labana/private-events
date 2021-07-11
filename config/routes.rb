e3d11b994666ae203981e3b19d6d3d6c32ef5faf
Omar Labana <omarlabana@outlook.com>
2021-07-11 23:54:01 +0200

    Update routing on actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+  
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'events#index'
+  delete 'attendances_from_creator/:user_id/:event_id', to: 'attendances#destroy', as: 'attendance_destroy'
+  delete 'attendances_from_attendee/:user_id/:event_id', to: 'attendances#destroy_from_attendee', as: 'attendance_unattend'
+  post 'attendances/:user_id/:event_id', to: 'attendances#create', as: 'create_attendance'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # resources :attendees, only: [:create, :destroy]
   resources :events
+  get '/users/:id', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'events#index'
 end
