00e7498c84e756943301dbb6fe43b4af805d5365
Omar Labana <omarlabana@outlook.com>
2021-07-13 03:29:33 +0200

    Add tests for event model and associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { in: 4..50 }
+  validates :location, presence: true, length: { maximum: 30 }
+  validates :date, presence: true
+
   belongs_to :creator, class_name: 'User'
   has_many :attendances, dependent: :delete_all
   has_many :attendees, through: :attendances, source: :user
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+# First collection
+class EventTest < ActiveSupport::TestCase
+  dummy_user = { username: 'omar', email: 'ramo@omar.com', password: 'omaruramo',
+                 password_confirmation: 'omaruramo' }
+  dummy_event = { title: 'ramo\'s poolpart!', date: Time.now, location: 'giza' }
+
+  RSpec.describe Event do
+    context 'checks for valid data' do
+      it 'creates an event' do
+        user1 = User.new(dummy_user)
+        user1.save
+        user1.events.create(dummy_event)
+        expect(Event.first.id).to eq(1)
+      end
+      it 'checks for invalid entries' do
+        user1 = User.new(dummy_user)
+        user1.save
+        invalid_event = Event.new(title: 'asdasd', date: Time.now, location: 's', creator_id: user1.id)
+        expect(invalid_event.valid?).to eq(false)
+      end
+      it 'checks associations' do
+        invalid_event = Event.new(dummy_event)
+        expect(invalid_event.valid?).to eq(false)
+      end
+      it 'returns correct error msg if associations are wrong' do
+        invalid_event = Event.new(dummy_event)
+        invalid_event.invalid?
+        expect(invalid_event.errors.full_messages).to eq(['Creator must exist'])
+      end
+    end
+  end
+end
