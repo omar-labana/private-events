2de536bdba60f1b39adb1423ea7c5651c3b6d495
Omar Labana <omarlabana@outlook.com>
2021-07-13 03:21:59 +0200

    Add validation and tests for user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  dummy_user = { username: 'omar', email: 'omar@ramo.com', password: 'omaruramo',
+                 password_confirmation: 'omaruramo' }
+  invalid_dummy_user = { username: 'aa', email: '@aa', password: 'pass', password_confirmation: 'omaruramo' }
+
+  RSpec.describe User do
+    context 'checking db validations' do
+      it 'creates a users' do
+        user1 = User.new(dummy_user)
+        expect(user1.valid?).to eq(true)
+      end
+      it 'rejects users' do
+        invalid_user = User.new(invalid_dummy_user)
+        expect(invalid_user.valid?).to eq(false)
+      end
+      it 'return correct error output' do
+        invalid_user = User.new(invalid_dummy_user)
+        invalid_user.valid?
+        error_msgs = ['Email is invalid',
+                      'Password confirmation doesn\'t match Password',
+                      'Password is too short (minimum is 6 characters)',
+                      'Username is too short (minimum is 3 characters)']
+        expect(invalid_user.errors.full_messages).to eq(error_msgs)
+      end
+      it 'return correct error output if user exists' do
+        User.create(dummy_user)
+        user1 = User.new(dummy_user)
+        expect(user1.valid?).to eq(false)
+      end
+      it 'return correct error output if user exists' do
+        User.create(dummy_user)
+        user1 = User.new(dummy_user)
+        user1.valid?
+        expect(user1.errors.full_messages).to eq(['Email has already been taken', 'Username has already been taken'])
+      end
+    end
+  end
+end
